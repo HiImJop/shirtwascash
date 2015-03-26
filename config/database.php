a456d65eeadf9e2a4b87f2df3670e5b963123c9c
TheHamsterOfGod <akosijimzter@gmail.com>
2015-03-26 15:14:29 +0800

    edited back: use .env to edit your database credentials

--- config/database.php
+++ config/database.php
@@ -55,10 +55,8 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			// 'database'  => env('DB_DATABASE', 'forge'),
-			'database'  => 'shirtwascash',
-			// 'username'  => env('DB_USERNAME', 'forge'),
-			'username'  => 'root',
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
