825b0196c152e6fd2a3e6c081fe791fdb7d1b2e5
Marion Serenio <marionserenio@bitbucket.org>
2015-03-26 13:11:53 +0800

    changed route to tshirtcontroller instead of welcome

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,8 +6,8 @@
 	<a href="/"><img src="{{ asset('/img/shirtwascash_logo.png') }}"></a>
 	<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
 		<input type="hidden" name="_token" value="{{ csrf_token() }}" class="input">
-		<input type="email" name="email" value="{{ old('email') }}" class="input">
-		<input type="password" name="password" class="input">
+		<input type="email" name="email" value="{{ old('email') }}" class="input" placeholder="email">
+		<input type="password" name="password" class="input"  placeholder="password">
 		<button type="submit" class="submit">Login</button>
 		<label class="remember_me"><input type="checkbox" name="remember"> Remember Me</label>
 		<a class="forgot_password" href="{{ url('/password/email') }}">Forgot Your Password?</a>
